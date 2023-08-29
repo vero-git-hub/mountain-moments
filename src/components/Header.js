492baa5512ff613e6fcdc36ba69d593a950d78a1
v_code <12273445-v_code@users.noreply.gitlab.com>
2023-08-29 17:32:37 +0300

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => (
-    <header>
-        <div className="logo">Mountaintop Moments </div>
-        <nav>
-            {/* Navigation items */}
+    <header className="header">
+        <div className="logo">Mountaintop Moments</div>
+        <nav className="navigation">
+            <ul>
+                <li><a href="#main-banner">Home</a></li>
+                <li><img src = "/separator.svg" alt="Separator"/></li>
+                <li><a href="#about-us">About</a></li>
+                <li><img src = "/separator.svg" alt="Separator"/></li>
+                <li><a href="#packages">Packages</a></li>
+            </ul>
+            <button className="cta-button">Contact</button>
         </nav>
     </header>
 );

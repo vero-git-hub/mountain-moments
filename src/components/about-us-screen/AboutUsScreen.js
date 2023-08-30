430cb8bd75b194b0cd0b35b003437573e35b0056
v_code <12273445-v_code@users.noreply.gitlab.com>
2023-08-30 17:16:05 +0300

    lightbox

--- src/components/about-us-screen/AboutUsScreen.js
+++ src/components/about-us-screen/AboutUsScreen.js
@@ -1,31 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AboutUsScreen.css';
 
-const AboutUsScreen = () => (
-	<div className="about-us-screen">
-		<h2>About Us</h2>
-		<div className="about-us-content">
-			<div className="text-box">
-				<p>
-					Mountaintop Moments - is a...
-					<ul>
-						<li>Majestic mountain scenery.</li>
-						<li>Comfortable chalets or cottages for living.</li>
+const AboutUsScreen = () => {
+	const [lightbox, setLightbox] = useState({ isOpen: false, imgSrc: '' });
+
+	const openLightbox = (src) => {
+		setLightbox({ isOpen: true, imgSrc: src });
+	};
+
+	const closeLightbox = () => {
+		setLightbox({ isOpen: false, imgSrc: '' });
+	};
+
+	return (
+		<div className="about-us-screen">
+			<h2>About Us</h2>
+			<div className="about-us-content">
+				<div className="text-box">
+					<p>
+						Mountaintop Moments - is a...
+						<ul>
+							<li>Majestic mountain scenery.</li>
+							<li>Comfortable chalets or cottages for living.</li>
 							<li>Activities available to tourists: skiing, snowmobiling, trekking, etc.</li>
-					</ul>
-					You will have something to remember!
-				</p>
+						</ul>
+						You will have something to remember!
+					</p>
+				</div>
+				<div className="image-grid">
+					<img src="/img/about-us/red-house.jpg" alt="Image 1" onClick={() => openLightbox("/img/about-us/red-house.jpg")}/>
+					<img src="/img/about-us/mountains.jpg" alt="Image 2"
+						 onClick={() => openLightbox("/img/about-us/mountains.jpg")}
+					/>
+					<img src="/img/about-us/mountain.jpg" alt="Image 3"
+						 onClick={() => openLightbox("/img/about-us/mountain.jpg")}
+					/>
+					<img src="/img/about-us/rocks.jpg" alt="Image 4"
+						 onClick={() => openLightbox("/img/about-us/rocks.jpg")}
+					/>
+					<img src="/img/about-us/lake.jpg" alt="Image 5"
+						 onClick={() => openLightbox("/img/about-us/lake.jpg")}
+					/>
+					<img src="/img/about-us/dog.jpg" alt="Image 6"
+						 onClick={() => openLightbox("/img/about-us/dog.jpg")}
+					/>
+				</div>
 			</div>
-			<div className="image-grid">
-				<img src="/img/about-us/red-house.jpg" alt="Image 1" />
-				<img src="/img/about-us/mountains.jpg" alt="Image 2" />
-				<img src="/img/about-us/mountain.jpg" alt="Image 3" />
-				<img src="/img/about-us/rocks.jpg" alt="Image 4" />
-				<img src="/img/about-us/lake.jpg" alt="Image 5" />
-				<img src="/img/about-us/dog.jpg" alt="Image 6" />
-			</div>
+			{lightbox.isOpen && (
+				<div className="lightbox" onClick={closeLightbox}>
+					<img src={lightbox.imgSrc} alt="Lightbox" />
+				</div>
+			)}
 		</div>
-	</div>
-);
+	);
+};
+
+
+
+// 	(
+// 	<div className="about-us-screen">
+// 		<h2>About Us</h2>
+// 		<div className="about-us-content">
+// 			<div className="text-box">
+// 				<p>
+// 					Mountaintop Moments - is a...
+// 					<ul>
+// 						<li>Majestic mountain scenery.</li>
+// 						<li>Comfortable chalets or cottages for living.</li>
+// 							<li>Activities available to tourists: skiing, snowmobiling, trekking, etc.</li>
+// 					</ul>
+// 					You will have something to remember!
+// 				</p>
+// 			</div>
+// 			<div className="image-grid">
+// 				<img src="/img/about-us/red-house.jpg" alt="Image 1" />
+// 				<img src="/img/about-us/mountains.jpg" alt="Image 2" />
+// 				<img src="/img/about-us/mountain.jpg" alt="Image 3" />
+// 				<img src="/img/about-us/rocks.jpg" alt="Image 4" />
+// 				<img src="/img/about-us/lake.jpg" alt="Image 5" />
+// 				<img src="/img/about-us/dog.jpg" alt="Image 6" />
+// 			</div>
+// 		</div>
+// 	</div>
+// );
 
 export default AboutUsScreen;

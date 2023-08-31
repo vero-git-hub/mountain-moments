7e0f2a8862526f40c18b9b6f59e5988ed5c229f8
v_code <12273445-v_code@users.noreply.gitlab.com>
2023-08-31 19:32:27 +0300

    slider

--- src/components/reviews/ReviewCard.js
+++ src/components/reviews/ReviewCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ReviewCard = ({ img, alt, text }) => (
 	<div className="slide">
-		<img src={img} alt={alt} />
+		<img src={img} alt={alt} loading="lazy"/>
 		<p>{text}</p>
 	</div>
 );
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,63 +1,95 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import './Reviews.css';
 import ReviewCard from './ReviewCard';
 
 const Reviews = () => {
+	const [currentIndex, setCurrentIndex] = useState(0);
 	const sliderRef = useRef(null);
+	const totalSlides = 8;
+	const slidesPerPage = 4;
+
 
 	const scroll = (direction) => {
 		if (sliderRef.current) {
-			sliderRef.current.scrollLeft += direction === 'left' ? -200 : 200;
+			const slideWidth = 276;
+			const gap = 20;
+			const numberOfSlidesToScroll = 4;
+			const offset = (slideWidth + gap) * numberOfSlidesToScroll;
+
+			const newIndex = direction === 'left' ? Math.max(0, currentIndex - slidesPerPage) : Math.min(totalSlides - slidesPerPage, currentIndex + slidesPerPage);
+			setCurrentIndex(newIndex);
+
+			sliderRef.current.style.transition = 'all 0.5s ease-in-out';
+			sliderRef.current.scrollLeft += direction === 'left' ? -offset : offset;
+
+			setTimeout(() => {
+				sliderRef.current.style.transition = 'none';
+			}, 500);
 		}
 	};
 
 	return (
 		<div className="reviews" id="reviews">
 			<h2>Summit Stories</h2>
-			<button className="arrow arrow-left" onClick={() => scroll('left')}>&lt;</button>
-			<div className="slider" ref={sliderRef}>
-				<ReviewCard
-					img="/img/slider/woman.jpg"
-					alt="Review 1"
-					text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
-				/>
-				<ReviewCard
-					img="/img/slider/people.jpg"
-					alt="Review 2"
-					text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
-				/>
-				<ReviewCard
-					img="/img/slider/sky.jpg"
-					alt="Review 3"
-					text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
-				/>
-				<ReviewCard
-					img="/img/slider/mountaineers.jpg"
-					alt="Review 4"
-					text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
-				/>
-				<ReviewCard
-					img="/img/slider/woman.jpg"
-					alt="Review 5"
-					text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
-				/>
-				<ReviewCard
-					img="/img/slider/people.jpg"
-					alt="Review 6"
-					text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
-				/>
-				<ReviewCard
-					img="/img/slider/sky.jpg"
-					alt="Review 7"
-					text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
-				/>
-				<ReviewCard
-					img="/img/slider/mountaineers.jpg"
-					alt="Review 8"
-					text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
-				/>
+			<div className="slider-container">
+				<button className="arrow arrow-left" onClick={() => scroll('left')}>&lt;</button>
+				<div className="slider" ref={sliderRef}>
+					<ReviewCard
+						img="/img/slider/woman.jpg"
+						alt="Review 1"
+						text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
+					/>
+					<ReviewCard
+						img="/img/slider/people.jpg"
+						alt="Review 2"
+						text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
+					/>
+					<ReviewCard
+						img="/img/slider/sky.jpg"
+						alt="Review 3"
+						text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
+					/>
+					<ReviewCard
+						img="/img/slider/mountaineers.jpg"
+						alt="Review 4"
+						text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
+					/>
+					<ReviewCard
+						img="/img/slider/woman.jpg"
+						alt="Review 5"
+						text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
+					/>
+					<ReviewCard
+						img="/img/slider/people.jpg"
+						alt="Review 6"
+						text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
+					/>
+					<ReviewCard
+						img="/img/slider/sky.jpg"
+						alt="Review 7"
+						text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
+					/>
+					<ReviewCard
+						img="/img/slider/mountaineers.jpg"
+						alt="Review 8"
+						text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
+					/>
+				</div>
+				<button className="arrow arrow-right" onClick={() => scroll('right')}>&gt;</button>
 			</div>
-			<button className="arrow arrow-right" onClick={() => scroll('right')}>&gt;</button>
+			<div className="slider-dots">
+				{Array.from({ length: Math.ceil(totalSlides / slidesPerPage) }).map((_, index) => (
+					<button
+						key={index}
+						className={`slider-dot ${currentIndex / slidesPerPage === index ? 'active' : ''}`}
+						onClick={() => {
+							setCurrentIndex(index * slidesPerPage);
+							sliderRef.current.scrollLeft = (276 + 20) * index * slidesPerPage;
+						}}
+					>
+					</button>
+				))}
+			</div>
 		</div>
 	);
 };
